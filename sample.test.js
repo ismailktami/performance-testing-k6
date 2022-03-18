67f215c02dc08e35ae128be44dcd5e8901813bae
Ismail Ktami <ismail.ktami@cdbdx.biz>
2022-03-18 02:43:56 +0100

    add setup and teardown functions

--- sample.test.js
+++ sample.test.js
@@ -1,10 +1,18 @@
 import http from 'k6/http';
 import { sleep,check } from 'k6';
 
+export function setup(){
+  console.log("SETUP");
+}
+
 export default function () {
   let res = http.get('https://test.k6.io');
   check(res,{
       "status is good":(r)=> r.status === 201
   })
   sleep(1);
+}
+
+export function teardown(data){
+  console.log("TEARDOWN");
 }
